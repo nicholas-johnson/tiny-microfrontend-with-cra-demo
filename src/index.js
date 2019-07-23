2124bd096fc1275aa443fba61585e5bf639b5019
Nicholas Johnson <me@nicholasjohnson.com>
2019-07-23 22:41:58 +0100

    Microapp"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.renderApp = id => {
+  ReactDOM.render(<App />, document.getElementById(id));
+};
+
+Array.from(document.getElementsByTagName("div")).forEach(div => {
+  div.onclick = () => {
+    window.renderApp(div.id);
+  };
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
